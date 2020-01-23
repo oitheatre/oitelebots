9af8b5150a426042858e8223166ecf789c721ed1
Vladislav Toigildin <vtoigildin@protonmail.com>
2020-01-23 19:04:03 +0300

    remove pyc

--- app/bot_template/bot.py
+++ app/bot_template/bot.py
@@ -16,13 +16,6 @@ def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
 def is_port_in_use(port):
     import socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -33,26 +26,29 @@ class Bot():
     def __init__(self):
         tgConfig = TgBotConfig()
         self.TOKEN = tgConfig.BOT_TOKEN
+        self.BOT_TGNAME = tgConfig.BOT_TGNAME
         self.DEBUG_MODE = True if os.environ['FLASK_ENV'] == 'development' else False
-
-        updater = Updater(TOKEN, use_context=True)
-
-        # Get the dispatcher to register handlers
-        dp = updater.dispatcher
-
-        # log all errors
-        dp.add_error_handler(error)
-
-        dp.add_handler(CommandHandler("start", start))
 
         # Start the Bot
         port = 8443
         url = 'https://telegram.oitheatre.ru:{}/{}'.format(port, self.TOKEN)
+
         if not self.DEBUG_MODE or not is_port_in_use(port):  # yes, this is weird; for FLASK_ENV development mode
+            logger.info('Starting bot {0} in {1} mode'.format(self.BOT_TGNAME, os.environ['FLASK_ENV']))
+            updater = Updater(self.TOKEN, use_context=True)
+
+            # Get the dispatcher to register handlers
+            dp = updater.dispatcher
+
+            # log all errors
+            dp.add_error_handler(error)
+
+            dp.add_handler(CommandHandler("start", start))
+
             updater.start_webhook(listen='127.0.0.1',
                                   port=port,
-                                  url_path=self.TOKEN,
-                                  clean=True)
+                                  url_path=self.TOKEN)
             updater.bot.set_webhook(url=url, certificate=open('cert.pem', 'rb'))
 
-            logger.info('running on {} port, TOKEN {}, url {}'.format(port, self.TOKEN, url))
+            logger.info('running bot {0} on {1} port, with TOKEN {2}, webhook url {3}'.format(self.BOT_TGNAME, port, self.TOKEN, url))
+            logger.info('you can check your webhook status here https://api.telegram.org/bot{0}/getWebhookInfo'.format(self.TOKEN))
